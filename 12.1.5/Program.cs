23c35cacd57621dc70c228b0e26ac3ad455fb571
SDozH <soldatov_alex@bk.ru>
2022-08-23 14:44:46 +0300

    Добавьте файлы проекта.

---
+++ 12.1.5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace _12._1._5
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Users> listUsers = new()
+        {
+           new Users() { Name = "Григорий", Login = Guid.NewGuid().ToString().Substring(1, 7), IsPremium = true },
+           new Users() { Name = "Василий", Login = Guid.NewGuid().ToString().Substring(1, 7), IsPremium = false },
+           new Users() { Name = "Милана", Login = Guid.NewGuid().ToString().Substring(1, 7), IsPremium = false },
+        };
+            foreach (var Prem in listUsers)
+            {
+                if (!Prem.IsPremium)
+                {
+                    Console.Write(Prem.Name + " ");
+                    Show.ShowAds();
+                }
+            }
+        }
+    }
+}
---
+++ 12.1.5/Users.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12._1._5
+{
+    public class Users
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public bool IsPremium { get; set; }
+
+    }
+}
+
+
